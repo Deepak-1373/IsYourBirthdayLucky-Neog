c4c1cb03898ec5a99e36fd45094c0e142312c337
Deepak Kamat <dkamat.001@gmail.com>
2021-09-19 19:41:26 +0530

    Feature: Fucntionality of Message display over lucky Number Complete

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ const calculateSum = (date) => {
   }
   return sum;
 };
+
+const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
+  console.log(sumOfDate, numberToCheck);
+  if (sumOfDate % numberToCheck === 0) {
+    return showMessage(`${numberToCheck} is a lucky number!! 🥳 🎉 `);
+  }
+  showMessage(`${numberToCheck} is not that lucky 😕`);
+};
+
+const showMessage = (message) => {
+  displayMessage.innerText = message;
+};
+
+checkButton.addEventListener("click", () => {
+  const date = dateOfBirth.value;
+  const numberToCheck = luckyNumber.value;
+  if (date && numberToCheck) {
+    const sumOfDate = calculateSum(date);
+    checkIsNumberLucky(sumOfDate, numberToCheck);
+  } else {
+    showMessage("Please enter both the fields");
+  }
+});
